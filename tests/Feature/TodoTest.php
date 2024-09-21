ece6dbbbc10681cffb7ccaf21f722760c541c3f1
Ray Lam <raylamedeas@gmail.com>
2024-09-21 18:49:04 +0100

    TDD: Add test for Todo model creation, expecting the test to fail

--- tests/Feature/TodoTest.php
+++ tests/Feature/TodoTest.php
@@ -13,11 +13,29 @@ class TodoTest extends TestCase
      */
     public function test_a_todo_can_be_created(): void
     {
-
         $response = $this->post('/todo', [
             'title' => 'My First TODO'
         ]);
 
         $this->assertEquals('My First TODO', $response['title']);
+    }
+
+    /**
+     * Test two Todos can be created.
+     */
+    public function test_two_todos_can_be_created(): void
+    {
+        $this->post('/todo', [
+            'title' => 'My First TODO'
+        ]);
+
+        $this->post('/todo', [
+            'title' => 'My Second TODO'
+        ]);
+
+        tap(Todo::latest(), function($todo){
+            $this->assertEquals('My Second TODO', $todo->title);
+        });
+
     }
 }

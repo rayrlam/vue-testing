489f91b7d56ec23a1a405b645f24c6759fa51260
Ray Lam <raylamedeas@gmail.com>
2024-09-21 18:19:03 +0100

    Add test for Alert component

--- resources/js/components/Alert.vue
+++ resources/js/components/Alert.vue
@@ -1,11 +1,21 @@
 <template>
-    <div>
- 
-    </div>
-  </template>
-  
-  <script>
-  export default {
- 
-  };
-  </script>
+  <div v-if="isVisible" data-testid="alert">
+    Alert content
+    <button @click="close" data-testid="close">Close</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      isVisible: true
+    }
+  },
+  methods: {
+    close() {
+      this.isVisible = false
+    }
+  }
+}
+</script>
--- resources/js/components/__tests__/Alert.test.js
+++ resources/js/components/__tests__/Alert.test.js
@@ -1,11 +1,30 @@
 import Alert from "../Alert.vue";
 
-import { shallowMount } from "@vue/test-utils"
+import { describe, it, expect } from 'vitest'
+
+import { mount } from "@vue/test-utils"
 
 describe('Alert', () => {
 
     it('is a component', () => {
-        const wrapper = shallowMount(Alert);
+        const wrapper = mount(Alert);
         expect(wrapper).toBeTruthy();
     });
+})
+
+describe('Close Button', () => {
+    it('closes when the user clicks the close button', async () => {
+        const wrapper = mount(Alert);
+
+        const alert = wrapper.find('[data-testid="alert"]')
+        expect(alert.exists()).toBe(true);
+
+        await wrapper.find('[data-testid="close"]').trigger('click');
+
+        // Wait for the next tick to ensure Vue has updated the DOM
+        await wrapper.vm.$nextTick();
+
+        // Check if the alert is no longer in the DOM
+        expect(wrapper.find('[data-testid="alert"]').exists()).toBe(false);
+    })
 })

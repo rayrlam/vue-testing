fa1bdc9a28a8fb4990f2d11c0a95bc92b5776123
Ray Lam <raylamedeas@gmail.com>
2024-09-21 18:36:09 +0100

    TDD: create test first before create the route of /todo

---
+++ tests/Feature/TodoTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class TodoTest extends TestCase
+{
+    /**
+     * Test a Todo can be created.
+     */
+    public function test_a_todo_can_be_created(): void
+    {
+
+        $response = $this->post('/todo', [
+            'title' => 'My First TODO'
+        ]);
+
+        $response->assertEquals('My First TODO', $response['title']);
+    }
+}

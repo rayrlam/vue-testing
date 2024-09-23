9a09263908a82ae16d6c41ac68a2538d0d241d97
Ray Lam <raylamedeas@gmail.com>
2024-09-23 20:23:00 +0100

    Setup Vue & Vue Router with Home & About pages

---
+++ resources/js/App.vue
@@ -0,0 +1,16 @@
+<template>
+    <h1>
+        Hello World!
+    </h1>
+    <router-link to="/">Home</router-link>
+    <router-link to="/about">About</router-link>
+    <router-view></router-view>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ resources/js/app.ts
@@ -0,0 +1,20 @@
+import { createApp } from 'vue';
+
+import {createRouter, createWebHashHistory} from 'vue-router';
+
+import routes from './routes';
+
+import './bootstrap';
+
+import App from './App.vue';
+
+const router = createRouter({
+    history: createWebHashHistory(),
+    routes
+});
+
+const app = createApp(App);
+
+app.use(router);
+
+app.mount("#app");

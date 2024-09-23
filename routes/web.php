c2a66c65e9a4ce597f73b5c010503e8bb087f7ff
Ray Lam <raylamedeas@gmail.com>
2024-09-23 18:38:37 +0100

    TDD: update TodoController@store route name and ensure tests pass

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::post('/todo', [TodosController::class,'store']);
+Route::post('/todo', [TodosController::class,'store'])->name('todo.store');
 Route::patch('/todo/{todo}', [TodosController::class, 'update'])->name('todo.update');
 Route::patch('/todo/{todo_id}/mark/completed', [TodosController::class, 'markCompleted']);

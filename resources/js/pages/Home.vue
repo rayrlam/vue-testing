b02acdebfd0156592d94b22127d5d3f1efadeff8
Ray Lam <raylamedeas@gmail.com>
2024-09-24 22:28:42 +0100

    Add luxon

--- resources/js/pages/Home.vue
+++ resources/js/pages/Home.vue
@@ -9,17 +9,18 @@
                 data-testid="todo-list-item"
             >{{ todo.title }}</li>
         </ul>
-
-        <button @click="todoStore.fetch()">Fetch Todos</button>
     </div>
 </template>
 
 <script setup lang="ts">
-    import axios from 'axios';
-    import { ref } from 'vue';
+    import { onBeforeMount, ref } from 'vue';
     import { useTodoStore } from '../stores/TodoStore';
 
     const todoStore = useTodoStore();
+
+    onBeforeMount(() => {
+        todoStore.fetch()
+    });
 </script>
 
 <style scoped>
--- resources/js/stores/TodoStore.ts
+++ resources/js/stores/TodoStore.ts
@@ -1,18 +1,25 @@
 import {defineStore} from 'pinia';
 import {ref} from 'vue';
 import axios from "axios";
+import {DateTime} from 'luxon';
 
 export const useTodoStore = defineStore('todos', () => {
     const todos = ref([]);
 
+    const lastFetch = ref(null);
+
     const fetch = async() => {
-        const response = await axios.get('todos');
-        todos.value = response.data.todos;
- 
+        const now = DateTime.now();
+        if(!lastFetch.value || lastFetch.value.plus({seconds:5}).toMillis() <= now.toMillis()){
+            const response = await axios.get('todos');
+            todos.value = response.data.todos;
+            lastFetch.value = now;
+        }
     };
 
     return {
         todos,
+        lastFetch,
         fetch
     };
 });

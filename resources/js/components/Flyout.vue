8fd33caa190fdf04825440dad1ec4265a9521cad
Ray Lam <raylamedeas@gmail.com>
2024-10-01 20:49:32 +0100

    TDD: add a flyout & test

---
+++ resources/js/components/Flyout.vue
@@ -0,0 +1,22 @@
+<template>
+    <div 
+        v-show="props.isOpen"
+        data-testid="flyout-backer"
+        @click="$event => emit('update:is-open', false)"
+    />
+
+    <div v-show="props.isOpen"><slot /></div>
+</template>
+
+<script setup lang="ts">
+
+const props = defineProps<{
+    isOpen: boolean;
+
+}>();
+
+const emit = defineEmits<{
+    (e: "update:is-open", boolean): void;
+}>();
+
+</script>

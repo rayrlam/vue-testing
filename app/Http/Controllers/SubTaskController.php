24ccd5c1352c22973a344215d24a0c3270e92cb6
Ray Lam <raylamedeas@gmail.com>
2024-10-05 14:30:20 +0100

    TDD: add SubTask model to fix {Expected response status code [201] but received 403.}

--- app/Http/Controllers/SubTaskController.php
+++ app/Http/Controllers/SubTaskController.php
@@ -10,6 +10,6 @@ class SubTaskController extends Controller
 {
     public function store(SubTaskRequest $request){
         $subtask = SubTask::create($request->validated());
-        return response()->json($subtask, 201);
+        return response()->json([], 201);
     }
 }
--- app/Http/Requests/SubTaskRequest.php
+++ app/Http/Requests/SubTaskRequest.php
@@ -11,7 +11,7 @@ class SubTaskRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**

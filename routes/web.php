91389f9ad26f8d288f6f7fd60d7adff4f65e279e
Ray Lam <raylamedeas@gmail.com>
2024-10-05 14:15:07 +0100

    TDD: add route subtask.store to fix {Route [subtask.store] not defined.}

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\TodosController;
+use App\Http\Controllers\SubTaskController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -13,3 +14,5 @@ Route::patch('/todo/{todo}', [TodosController::class, 'update'])->name('todo.upd
 Route::patch('/todo/{todo_id}/mark/completed', [TodosController::class, 'markCompleted'])->name('todo.completed');
 Route::patch('/todo/{todo_id}/mark/uncomplete', [TodosController::class, 'markUncomplete'])->name('todo.uncomplete');
 Route::delete('/todo/{todo}', [TodosController::class,'archive'])->name('todo.archive');
+
+Route::post('/todo/{todo}/subtask', [SubTaskController::class,'store'])->name('subtask.store');

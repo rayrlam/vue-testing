8f3311860e1751a9570064531e5cd1d82f4556ec
Ray Lam <raylamedeas@gmail.com>
2024-10-05 14:22:26 +0100

    TDD: add store method to fix {Call to undefined method App\Http\Controllers\SubTaskController::store()}

--- app/Http/Controllers/SubTaskController.php
+++ app/Http/Controllers/SubTaskController.php
@@ -3,8 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Requests\SubTaskRequest;
+use App\Models\SubTask;
 
 class SubTaskController extends Controller
 {
-    //
+    public function store(SubTaskRequest $request){
+        $subtask = SubTask::create($request->validated());
+        return response()->json($subtask, 201);
+    }
 }
